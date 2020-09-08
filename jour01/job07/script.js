de5e95cd435d4b48529d04a8f8476e47ff7e2d5b
Baktash Waqibeen <baktash.waqibeen@laplateforme.io>
2020-09-08 09:47:23 +0200

    js

---
+++ jour01/job07/script.js
@@ -0,0 +1,11 @@
+
+function afficherjourssemaines()
+{
+	semaine = ["Lundi", "Mardi","Mercredi", "Jeudi", "Vendredi", "Samedi","Dimanche"];
+	for(i=0;i<semaine.length;i++)
+	{
+		console.log(semaine[i]);
+	}
+}
+
+afficherjourssemaines();
